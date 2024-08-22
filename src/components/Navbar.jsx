02bae4a6b38d50f02682203458e559a783384c85
Atharva <atharvakhangar14@gmail.com>
2024-08-22 21:06:15 +0530

    styled the hamburger button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Navbar.css'
 import { Link } from 'react-router-dom'
+import {FaBars, FaTimes} from 'react-icons/fa'
+
 const Navbar = () => {
+
+    const[click,setClick] = useState(false)
+
+    const handleClick = () => setClick(!click)
+
   return (
     <div className='header'>
         <Link to="/"><h1>Portfolio</h1></Link>
@@ -19,9 +26,14 @@ const Navbar = () => {
                 <Link to="/contact">Contact</Link>
             </li>
         </ul>
-        <div className="hamburger">
-            <faBars />
-        </div>
+        <div className="hamburger" onClick={handleClick}>
+  {click ? (
+    <FaTimes size={20} style={{ color: "#fff", cursor: "pointer" }} className="hamburger-icon" />
+  ) : (
+    <FaBars size={20} style={{ color: "#fff", cursor: "pointer" }} className="hamburger-icon" />
+  )}
+</div>
+
     </div>
   )
 }
