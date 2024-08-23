88e28b54163b4fb04fe4271aaec2da2ff097a7a8
Atharva <atharvakhangar14@gmail.com>
2024-08-23 15:43:48 +0530

    hero section + navbar bug correction

---
+++ src/components/Heroimg.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../styles/Heroimg.css';
+import IntroImg from '../assets/intro-bg.jpg';
+import { Link } from 'react-router-dom';
+
+const Heroimg = () => {
+  return (
+    <div className='hero'>
+      <div className='mask'></div>
+      <img className="intro-img" src={IntroImg} alt="intro-img" />
+      <div className='content'>
+            <p>HI! I AM A DEVELOPER</p>
+            <h1>REACT DEVELOPER</h1>
+            <div>
+                <Link to="/project" className='btn'>Projects</Link>
+                <Link to="/contact" className='btn-light'>Contact</Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Heroimg
