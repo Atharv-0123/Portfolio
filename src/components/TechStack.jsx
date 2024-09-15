8ae8e2a2bc7d0143885e3ba10b1c9006c1265386
Atharva <atharvakhangar14@gmail.com>
2024-09-15 23:52:22 +0530

    finished techstack

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import images from './TechStackData'
+import React from 'react';
+import images from './TechStackData';
+import '../styles/TechStack.css';
 
 const TechStack = () => {
   return (
-    <div className='techstack-container'>
-        <div className='icon'>
+    <div className='techstack-section'>
+      <h1 className='techstack-heading'>Tech Stack</h1>
+      <div className='techstack-container'>
         {images.map((image, index) => (
-        <img key={index} src={image} alt={`Image ${index + 1}`} />
-      ))}
-        </div>
+          <img 
+            key={index} 
+            src={image} 
+            alt={`Tech Stack Icon ${index + 1}`} 
+            className='techstack-icon'
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TechStack
+export default TechStack;
--- src/routes/About.js
+++ src/routes/About.js
@@ -3,13 +3,14 @@ import  Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import HeroImg2 from '../components/Heroimg2'
 import AboutContent from '../components/AboutContent'
-
+import TechStack from '../components/TechStack'
 const About = () => {
   return (
     <div>
       <Navbar/>
       <HeroImg2 heading="ABOUT" text="Info about me"/>
       <AboutContent/>
+      <TechStack/>
       <Footer/>
     </div>
   )
