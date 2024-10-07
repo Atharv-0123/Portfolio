a8f9062bb8860d4bb72bf91d783ac39447b9886d
Atharva <atharvakhangar14@gmail.com>
2024-10-07 11:46:49 +0530

    updated cv link

--- src/components/Heroimg.jsx
+++ src/components/Heroimg.jsx
@@ -13,7 +13,7 @@ const Heroimg = () => {
             <h1>Atharva Khangar</h1>
             <div>
                 <Link to="/projects" className='btn'>Projects</Link>
-                <Link to="https://drive.google.com/file/d/1IyzcnowQHG9RIFgxSp0c0OOQzE1tVf4F/view?usp=sharing" className='btn-light' target="_blank" rel="noopener noreferrer">Resume</Link>
+                <Link to="https://drive.google.com/file/d/1FUAzKtIWPdzpGeWGEAB3MLidVpW-tJux/view?usp=drive_link" className='btn-light' target="_blank" rel="noopener noreferrer">Resume</Link>
             </div>
         </div>
     </div>
